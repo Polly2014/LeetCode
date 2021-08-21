1c129b75ff454c2addb5c1f22e2dac35e043b516
Polly <26716201@qq.com>
2021-08-21 23:07:41 +0800

    刷刷更健康 2021.08.21 (python is a bug in LeetCode)

--- 39_Combination_Sum.py
+++ 39_Combination_Sum.py
@@ -2,25 +2,47 @@
 # @Author: Polly
 # @Date:   2021-08-21 21:52:05
 # @Last Modified by:   Polly
-# @Last Modified time: 2021-08-21 22:15:27
+# @Last Modified time: 2021-08-21 23:04:35
 from typing import List
 
 
 class Solution:
+    # def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    #     n = len(candidates)
+    #     ans = []
+
+    #     def DFS(idx, combine, res):
+    #         if idx >= n or res >= target:
+    #             if res == target:
+    #                 ans.append(combine[:])
+    #             return
+    #         combine.append(candidates[idx])
+    #         DFS(idx, combine, res + candidates[idx])
+    #         combine.pop()
+    #         DFS(idx + 1, combine, res)
+    #     DFS(0, [], 0)
+    #     print(ans)
+
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         n = len(candidates)
         ans = []
+        path = []
 
-        def DFS(idx, combine, res):
-            if idx >= n or res >= target:
-                if res == target:
-                    ans.append(combine[:])
+        def BackTracking(candidates, target, sum_path, startIndex):
+            if sum_path > target:
                 return
-            combine.append(candidates[idx])
-            DFS(idx, combine, res + candidates[idx])
-            combine.pop()
-            DFS(idx + 1, combine, res)
-        DFS(0, [], 0)
+            if sum_path == target:
+                ans.append(path[:])
+                return
+            for i in range(startIndex, n):
+                if sum_path + candidates[i] > target:
+                    return
+                sum_path += candidates[i]
+                path.append(candidates[i])
+                BackTracking(candidates, target, sum_path, i)
+                sum_path -= candidates[i]
+                path.pop()
+        BackTracking(candidates, target, 0, 0)
         print(ans)
 
 
