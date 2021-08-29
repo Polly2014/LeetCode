7f46230a41233020acfafce1c489c150bd0da345
Polly <26716201@qq.com>
2021-08-29 21:51:47 +0800

    刷刷更健康 2021.08.29 (python is a Magic in LeetCode)

--- 011_Max_Area.py
+++ 011_Max_Area.py
@@ -2,7 +2,7 @@
 # @Author: Polly
 # @Date:   2021-08-29 21:41:37
 # @Last Modified by:   Polly
-# @Last Modified time: 2021-08-29 21:42:06
+# @Last Modified time: 2021-08-29 21:46:52
 from typing import List
 
 
@@ -10,3 +10,17 @@ class Solution:
     def maxArea(self, height: List[int]) -> int:
         n = len(height)
         l, r = 0, n - 1
+        ans = 0
+        while l <= r:
+            area = min(height[l], height[r]) * (r - l)
+            ans = max(ans, area)
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return ans
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
--- 1588_Sum_Odd_Length_Subarrays.py
+++ 1588_Sum_Odd_Length_Subarrays.py
@@ -2,7 +2,7 @@
 # @Author: Polly
 # @Date:   2021-08-29 20:21:45
 # @Last Modified by:   Polly
-# @Last Modified time: 2021-08-29 20:57:24
+# @Last Modified time: 2021-08-29 21:45:08
 from typing import List
 
 
@@ -16,7 +16,7 @@ class Solution:
             right_even = int((right + 1) / 2)
             left_odd = int(left / 2)
             right_odd = int(right / 2)
-            print('i={}, ans={}'.format(i, (left_even * right_even + left_odd * right_odd) * arr[i]))
+            # print('i={}, ans={}'.format(i, (left_even * right_even + left_odd * right_odd) * arr[i]))
             ans += (left_even * right_even + left_odd * right_odd) * arr[i]
         return int(ans)
 
