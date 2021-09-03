629b12ea9a72cac98274f5c6f17f62bc51816f73
Polly <26716201@qq.com>
2021-09-04 00:26:44 +0800

    刷刷更健康 2021.09.04 (python is a Magic in LeetCode)

--- 098_Is_Valid_BST.py
+++ 098_Is_Valid_BST.py
@@ -2,7 +2,7 @@
 # @Author: Polly
 # @Date:   2021-09-03 23:37:47
 # @Last Modified by:   Polly
-# @Last Modified time: 2021-09-03 23:59:15
+# @Last Modified time: 2021-09-04 00:26:36
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -27,3 +27,15 @@ class Solution:
             if ans[i] <= ans[i - 1]:
                 return False
         return True
+
+    def isValidBST(self, root: TreeNode) -> bool:
+        def BFS(root, left, right):
+            if root is None:
+                return True
+
+            if left < root.val < right:
+                return BFS(root.left, left, root.val) and BFS(root.right, root.val, right)
+            else:
+                return False
+
+        return BFS(root, -float('inf'), float('inf'))
