38572b369587f963587bf17aa3cecae8917469fd
Polly <26716201@qq.com>
2021-09-04 17:14:57 +0800

    刷刷更健康 2021.09.04 (python is a Magic in LeetCode)

---
+++ 116_Connect.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# @Author: Polly
+# @Date:   2021-09-04 17:06:15
+# @Last Modified by:   Polly
+# @Last Modified time: 2021-09-04 17:11:27
+from typing import List
+# Definition for a Node.
+
+
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return []
+        ans = []
+        q = []
+        while q:
+            n = len(q)
+            for i in range(n):
+                node = q.pop(0)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                if i == n - 1:
+                    break
+                node.next = q[0]
+        return root
