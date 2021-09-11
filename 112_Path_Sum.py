4211d30ec292928ce6740843ce88eca6c83805dd
Polly <26716201@qq.com>
2021-09-11 22:28:37 +0800

    刷刷更健康 2021.09.11 (python is a Magic in LeetCode)

--- 112_Path_Sum.py
+++ 112_Path_Sum.py
@@ -2,7 +2,7 @@
 # @Author: Polly
 # @Date:   2021-08-20 10:44:17
 # @Last Modified by:   Polly
-# @Last Modified time: 2021-08-26 11:08:08
+# @Last Modified time: 2021-09-11 22:01:22
 from typing import List
 # Definition for a binary tree node.
 
@@ -38,8 +38,11 @@ class Solution:
 
         # 方法二：DFS
         def DFS(node, count):
+            # 空节点判断
             if not root:
                 return
+            # 叶子节点判断
             if not root.left and not root.right and count == root.val:
                 return True
+            # 问题规模可缩小，DFS表示当前传入节点为根节点，到其叶子节点是否存在路径和为传入的长度值（返回True|False）
             return DFS(root.left, count - root.val) or DFS(root.right, count - root.val)
---
+++ 600_Find_Integers.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Author: Polly
+# @Date:   2021-09-11 21:59:31
+# @Last Modified by:   Polly
+# @Last Modified time: 2021-09-11 22:27:45
+
+class Solution:
+    def findIntegers(self, n: int) -> int:
+        n_bin = bin(n)[2:]
+        N = len(n_bin)
+        dp = [0 for _ in range(N + 1)]
+        dp[0], dp[1] = 1, 2
+        for i in range(2, N + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        ans = 0
+        for i, num in enumerate(n_bin):
+            if num == '1':
+                ans += dp[N - i - 1]
+                if i > 0 and n_bin[i - 1] == '1':
+                    return ans
+        return ans + 1
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.findIntegers(5))
