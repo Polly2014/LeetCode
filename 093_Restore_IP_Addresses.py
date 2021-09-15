92d132a2bf077e96e46022bd69c90850aa3bad5a
Polly <26716201@qq.com>
2021-09-15 23:21:16 +0800

    刷刷更健康 2021.09.15 (python is a Magic in LeetCode)

--- 093_Restore_IP_Addresses.py
+++ 093_Restore_IP_Addresses.py
@@ -2,9 +2,17 @@
 # @Author: Polly
 # @Date:   2021-09-14 23:32:54
 # @Last Modified by:   Polly
-# @Last Modified time: 2021-09-14 23:33:02
+# @Last Modified time: 2021-09-15 17:27:10
 from typing import List
 
 
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
+        ans,path = [],[]
+        def BackTracking(s, startIndex):
+            if startIndex==len(s):
+                if is_valid(path):
+                    ans.append(path[:])
+                return
+            for i in range(startIndex, len(s)):
+                pass
---
+++ 162_Find_Peak_Element.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Author: Polly
+# @Date:   2021-09-15 23:13:04
+# @Last Modified by:   Polly
+# @Last Modified time: 2021-09-15 23:15:44
+from typing import List
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+    	nums.append(-float('inf'))
+    	n = len(nums)
+    	l,r = 0, n-2
+    	while l<=r:
+    		mid = (l+r)>>1
+    		if nums[mid]<nums[mid+1]:
+    			l = mid+1
+    		else:
+    			r = mid-1
+    	return l
+if __name__=='__main__':
+	s = Solution()
+	print(s.findPeakElement([1,2,1,3,5,6,4]))
