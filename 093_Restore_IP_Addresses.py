395c8d1c6d34f70f536e6b5e4a6bcd6397006f80
Polly <26716201@qq.com>
2021-09-15 16:40:53 +0800

    刷刷更健康 2021.09.15 (python is a Magic in LeetCode)

---
+++ 093_Restore_IP_Addresses.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: Polly
+# @Date:   2021-09-14 23:32:54
+# @Last Modified by:   Polly
+# @Last Modified time: 2021-09-14 23:33:02
+from typing import List
+
+
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
