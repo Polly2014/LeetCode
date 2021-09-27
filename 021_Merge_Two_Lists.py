fca88b7118a1ec30b83919de8fa3848291c98c12
Polly <26716201@qq.com>
2021-09-27 10:31:26 +0800

    刷刷更健康 2021.09.27 (python is a Magic in LeetCode)

--- 021_Merge_Two_Lists.py
+++ 021_Merge_Two_Lists.py
@@ -2,7 +2,7 @@
 # @Author: Polly
 # @Date:   2021-09-17 16:46:47
 # @Last Modified by:   Polly
-# @Last Modified time: 2021-09-17 16:50:40
+# @Last Modified time: 2021-09-27 10:23:07
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -11,6 +11,7 @@ class ListNode:
 
 
 class Solution:
+    # 递归
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         if not l1:
             return l2
@@ -22,3 +23,20 @@ class Solution:
         if l1.val <= l2.val:
             l1.next = self.mergeTwoLists(l1.next, l2)
             return l1
+
+
+
+    # 迭代
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode(-1)
+        prev = dummy
+        while l1 and l2:
+            if l1.val<=l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+        prev.next = l1 if l1 else l2
+        return dummy.next
