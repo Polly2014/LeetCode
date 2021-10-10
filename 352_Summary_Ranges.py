757c93be7abcc2ad34e7e3c13317266c35d0055a
Polly <26716201@qq.com>
2021-10-10 12:16:41 +0800

    刷刷更健康 2021.10.10 (python is a Magic in LeetCode)

--- 352_Summary_Ranges.py
+++ 352_Summary_Ranges.py
@@ -2,12 +2,48 @@
 # @Author: Polly
 # @Date:   2021-10-09 21:15:27
 # @Last Modified by:   Polly
-# @Last Modified time: 2021-10-09 21:45:48
+# @Last Modified time: 2021-10-10 11:43:38
 from typing import List
 from sortedcontainers import SortedSet
 
-class SummaryRanges:
 
+class SummaryRanges:
+    # 并查集
+    def __init__(self):
+        self.father = [-1 for i in range(10001)]
+
+    def addNum(self, val: int) -> None:
+        if self.father[val] == -1:
+            self.father[val] = val
+            self.union(val, val + 1)
+            self.union(val - 1, val)
+
+    def find(self, x):
+        if self.father[x] != x:
+            self.father[x] = self.find(self.father[x])
+        return self.father[x]
+
+    def union(self, x, y):
+        if 0 <= x <= 10000 and self.father[x] != -1 and self.father[y] != -1:
+            father_x = self.find(x)
+            father_y = self.find(y)
+            if father_x != father_y:
+                self.father[father_x] = father_y
+
+    def getIntervals(self) -> List[List[int]]:
+        ans = []
+        i = 0
+        while i < 10001:
+            if self.father[i] != -1:
+                start = i
+                end = self.find(i)
+                ans.append([start, end])
+                i = end + 1
+            else:
+                i += 1
+        return ans
+
+    '''经典模拟
     def __init__(self):
         self.mask = [False] * 10001
 
@@ -31,8 +67,8 @@ class SummaryRanges:
         if start != -1:
             ans.append([start, end])
         return ans
-
-        # Your SummaryRanges object will be instantiated and called as such:
-        # obj = SummaryRanges()
-        # obj.addNum(val)
-        # param_2 = obj.getIntervals()
+    '''
+    # Your SummaryRanges object will be instantiated and called as such:
+    # obj = SummaryRanges()
+    # obj.addNum(val)
+    # param_2 = obj.getIntervals()
